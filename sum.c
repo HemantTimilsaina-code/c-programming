cfe32f34e0118a5d55ec66ebe2d0934e330af8da
HemantTimilsaina-code <bvevibe@gmail.com>
2021-01-03 13:30:28 +0545

    sum function

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int a,b,sum;
+ printf("enter the num");
+ scanf("%d", &a);
+ printf("enter the second num");
+ scanf("%d", &b);
+ sum=a+b;
+ printf("sum of two num %d",sum);
+ 
+}
